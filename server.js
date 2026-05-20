a37ae64a98e85dc62184c7310e808ff8bdc18417
Le-Sourcier <hackersranch@gmail.com>
2026-05-20 11:34:00 +0000

    Add cPanel Next server

---
+++ server.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { createServer } = require("http");
+const next = require("next");
+
+const dev = false;
+const hostname = process.env.HOST || process.env.HOSTNAME || "127.0.0.1";
+const port = Number(process.env.PORT || 3000);
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, hostname, () => {
+    console.log(`Ready on http://${hostname}:${port}`);
+  });
+});
